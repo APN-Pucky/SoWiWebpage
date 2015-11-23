84279af819ddf9cd5ef4949f0b82613eeeedff19
APN-Pucky <APN-Pucky@users.noreply.github.com>
2015-11-23 09:17:51 +0100

    Unterricht Gedönz

--- bot.js
+++ bot.js
@@ -1,13 +1,16 @@
 function println(text) {
+	//add filter
 	document.getElementById('text').innerHTML += text + "<br>";
 }
 function say(){
 	var input = document.getElementById('input');
 	println("You: " + input.value);
+	respond(input.value);
 	input.value = "";
-	
 }
-function respond(){}
+function respond(text) {
+	println("Bot: You said: " + text);
+}
 function init() {
 	var button = document.getElementById('say');	
 	button.addEventListener('click', say,true);
