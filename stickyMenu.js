1f2f7329a9f72004cb68ee5a2dda09305963e989
dfb159 <dfb159@users.noreply.github.com>
2015-12-19 19:59:49 +0100

    nav leiste nach header
    color like official page nav color

--- stickyMenu.js
+++ stickyMenu.js
@@ -1,6 +1,6 @@
 // Create a clone of the menu, right next to original.
 function initSticky() {
-	$('.menu').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
+	$('.menu').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','2').css('z-index','500').removeClass('original').hide();
 
 	scrollIntervalID = setInterval(stickIt, 10);
 }
@@ -10,7 +10,7 @@ function stickIt() {
   var orgElementPos = $('.original').offset();
   orgElementTop = orgElementPos.top;               
 
-  if ($(window).scrollTop() >= (orgElementTop)) {
+  if ($(window).scrollTop() >= (orgElementTop) - 2) {
     // scrolled past the original position; now only show the cloned, sticky element.
 
     // Cloned element should always have same left position and width as original element.     
