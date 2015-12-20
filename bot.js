971f66ff39079570cbbdca730f90f03ed083221e
APN-Pucky <APN-Pucky@users.noreply.github.com>
2015-12-20 03:45:46 +0100

    bot improved

--- bot.js
+++ bot.js
@@ -1,29 +1,33 @@
-function println(text) {
-	//add filter
-	text = text.toString().replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
-	var textfield = document.getElementById('text');
-	textfield.innerHTML += text + "<br>";
-	textfield.scrollTo(0,textfield.scrollHeight);
+function escape(text) {
+	return text.toString().replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;") + "<br>";
 	
+}
+function printlnUnescaped(text) {
+	var textfield = document.getElementById('text');
+        textfield.innerHTML += text;
+        textfield.scrollTo(0,textfield.scrollHeight);
 }
 function say(){
 	var input = document.getElementById('input');
-	println("You: " + input.value);
+	printlnUnescaped("<font color='blue'>"+"You: " + escape(input.value)+"</font>");
 	respond(input.value);
 	input.value = "";
 }
 function sayBot(text) {
-	println("Bot: " + text);
+	printlnUnescaped("<font color='red'>"+"Bot: " + escape(text) + "</font>");
 }
+//run level.....1,2,3,4
 function respond(text) {
-	sayBot(text);
-	sayBot("You use: " + navigator.userAgent.match(/\w*/)[0]);
-	sayBot("on       " + navigator.platform.match(/\w*/)[0]);
+	//sayBot(text);
+	sayBot("Ist das dein Ernst!?	Wie kann man nur so dumm sein?!?");	
+	sayBot("Jedes Kind weiß doch, dass ...");
+	sayBot("Was machst du noch auf meiner Seite?	Abschaum wie dich kann ich hier nicht brauchen!");
 }
+//themen: Klimakrise, Weltpolitik, Religion, Geschmack(Film, Musik) -------->>> shitstorm
 function init() {
 	var button = document.getElementById('say');	
 	button.addEventListener('click', say,true);
-	sayBot("Lol, du Spast!");
+	sayBot("Hallo, was ist deine Meinung zu" + "Thema");
 }
 
 
